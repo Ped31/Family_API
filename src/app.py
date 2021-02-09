bf92495250ac7e7a2efbaef7aa844f3fc052c609
Ped31 <pedrorivdom@gmail.com>
2021-02-09 20:01:49 +0000

    Todos los test pasados

--- src/app.py
+++ src/app.py
@@ -6,39 +6,73 @@ from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
-#from models import Person
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
 
-# create the jackson family object
+
 jackson_family = FamilyStructure("Jackson")
 
-# Handle/serialize errors like a JSON object
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
 def handle_hello():
+    try:
+        members = jackson_family.get_all_members()
+        return jsonify(members), 200
+    except:
+        return jsonify('Server Error'), 500
 
-    # this is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+@app.route('/member/<int:id>', methods=['GET'])
+def get_single_member(id):
+    user = jackson_family.get_member(id)
+    if user:
+        return user, 200
+    return 404
+
+@app.route('/member/<int:id>', methods=["DELETE"])
+def delete_member(id):
+    user = jackson_family.delete_member(id)
+    if user:
+        return jsonify({"done": True}), 200
+    
+    return jsonify('Bad request'), 404
+  
+
+@app.route('/member', methods=["POST"])
+def cretate_member():
+    try:
+        member = {
+            "id": request.json["id"],
+            "first_name": request.json["first_name"],
+            "last_name": "Jackson",
+            "age": request.json["age"],
+            "lucky_numbers": request.json["lucky_numbers"]
+        }
+        create = jackson_family.add_member(member)
+        if(create == 200):
+            return jsonify('All good'), 200
+
+    except ValueError as err:
+        return jsonify('Server Error'), 500
+
+    except Exception as err:
+        return jsonify('Bad Request'), 405
+
+    except:
+        return jsonify('Internal server error'), 500
 
 
-    return jsonify(response_body), 200
-
-# this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=True)
